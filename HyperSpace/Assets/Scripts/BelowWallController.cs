04eb98dcc04f7ad60a962f10ab784d2de6f846f7
Dmk <dogaminekaba@gmail.com>
2016-07-14 20:13:49 +0300

    Grids are added

---
+++ HyperSpace/Assets/Scripts/BelowWallController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BelowWallController : MonoBehaviour {
+
+    public float speed;
+    private float nextWall = 0.0F;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.z < -18)
+        {
+            Destroy(this.gameObject);
+        }
+        transform.position += new Vector3(0, 0, -speed);
+	}
+}
