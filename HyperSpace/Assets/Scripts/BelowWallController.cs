f4830c3dea65670ce95d13d5753799a12b1d3eaa
Dmk <dogaminekaba@gmail.com>
2016-07-15 17:21:33 +0300

    First View Wall Instantiation
    Generating automated walls and accelerating speed by time is done for
    View 1

--- HyperSpace/Assets/Scripts/BelowWallController.cs
+++ HyperSpace/Assets/Scripts/BelowWallController.cs
@@ -3,21 +3,14 @@ using System.Collections;
 
 public class BelowWallController : MonoBehaviour {
 
-    public float speed;
-    private float nextWall = 0.0F;
+    static public float speed;
+    private Rigidbody rb;
 
 	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (transform.position.z < -18)
-        {
-            Destroy(this.gameObject);
-        }
-        transform.position += new Vector3(0, 0, -speed);
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // move backwards
+        rb.velocity = speed * transform.forward * -1;
+    }
 }
---
+++ HyperSpace/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject wall;
+    public Vector3 spawnValues;
+    public float startWait;
+    public float speed;
+    public float range;
+    public float maxSpeed;
+
+    void Start()
+    {
+        BelowWallController.speed = speed;
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(wall, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(range / speed);
+            if(speed < maxSpeed)
+            {
+                speed += 0.5F;
+                BelowWallController.speed = speed;
+            }
+        }
+    }
+}
